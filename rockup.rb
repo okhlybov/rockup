33f56d66adfb10c592d0de374be8aec3930d0731
Oleg A. Khlybov <fougas@mail.ru>
2018-04-17 15:43:23 +0500

    Implemented recording of the checksum/stats info.

--- rockup.rb
+++ rockup.rb
@@ -31,7 +31,7 @@ class Project
 		end
 		source.each_value do |source|
 			source.files.each do |file|
-				manifest.merge!(source, file, volume.merge!(source, file))
+				manifest.merge!(file, volume.merge!(file))
 			end
 		end
 		begin
@@ -69,35 +69,68 @@ class Source
 	# Append all files recursively contained in root into list
 	# Stored file names are relative to root
 	private def scan(list, root, path = nil)
-		Dir.entries(path.nil? ? root : File.join(root, path)).each do |entry|
+		Dir.entries(path.nil? ? root : ::File.join(root, path)).each do |entry|
 			next if entry == '.' || entry == '..'
-			relative = path.nil? ? entry : File.join(path, entry)
-			full = File.join(root, relative)
-			if File.directory?(full)
+			relative = path.nil? ? entry : ::File.join(path, entry)
+			full = ::File.join(root, relative)
+			if ::File.directory?(full)
 				scan(list, root, relative)
 			else
-				list << relative
+				list << File.new(self, relative)
 			end
 		end
 		list
 	end
+
+	# Represents single file relative to the source
+	class File < String
+
+		attr_reader :source
+
+		def initialize(source, name)
+			super(name)
+			@source = source
+			@path = ::File.join(self.source.directory, self)
+		end
+
+		def encode_with(coder)
+			coder.represent_map(nil, to_h)
+		end
+
+		def to_h
+			{to_s => {'size' => stat.size, 'mtime' => stat.mtime, 'sha1' => sha1}}
+		end
+
+		def sha1
+			@sha1.nil? ? @sha1 = Digest::SHA1.file(@path).to_s : @sha1
+		end
+
+		def stat
+			@stat.nil? ? @stat = ::File::Stat.new(@path) : @stat
+		end
+
+	end # File
 
 end # Source
 
 
 class Volume
 
-	attr_reader :id, :project
+	attr_reader :id, :project,
+		:path # Path to the volume root directory
 
 	def modified?; @modified end
 
 	def initialize(project, tag = nil)
 		@project = project
 		@modified = false
-		@contents = []
+		@contents = [] # Array of Stream instances
+		# Perform cleanup actions on rollback
+		# Note then setting this to true might leave the on-disk data in inconsistent state
+		@cleanup = true
 		if tag.nil?
 			@new = true
-			@id = Time.now.to_i
+			@id = Time.now.to_i # Leave integer to prevent quotation of the number-like string by YAML emitter
 			@path = File.join(project.destination, id.to_s)
 		else
 			@new = false
@@ -109,12 +142,11 @@ class Volume
 
 	def store!
 		if modified?
-			raise 'Refuse to overwrite existing volume' if @new && File.exist?(@path)
+			raise 'Refuse to overwrite existing volume' if @new && File.exist?(path)
 			begin
 				@contents.each do |stream|
-					FileUtils.mkdir_p(basedir = File.join(@path, File.dirname(stream.id)))
-					FileUtils.cp_r(File.join(stream.source.directory, stream.file), destfile = File.join(@path, stream.id))
-					stream.sha1 = Digest::SHA1.file(destfile).to_s
+					FileUtils.mkdir_p(basedir = File.join(path, File.dirname(stream.id)))
+					FileUtils.cp_r(File.join(stream.file.source.directory, stream.file), File.join(path, stream.id))
 				end
 			rescue
 				cleanup!
@@ -123,47 +155,51 @@ class Volume
 		end
 	end
 
-	def merge!(source, file)
+	# Create and register new Stream instance for the specified Source::File instance
+	def merge!(file)
 		raise 'Refuse to modify existing volume' unless @new
 		@modified = true
-		@contents << (stream = Stream.new(self, source, file))
+		@contents << (stream = Stream.new(self, file))
 		stream
 	end
 
 	def cleanup!
-		FileUtils.rm_rf(@path) if modified?
+		FileUtils.rm_rf(@path) if modified? && @cleanup
 	end
 
-	class Stream
+	class Stream < String
 
-		attr_reader :id, :volume, :source, :file
+		attr_reader :id, :volume, :file
 
 		attr_accessor :sha1
 
-		def initialize(volume, source, file)
-			@file = file
+		def initialize(volume, file)
+			super(file)
 			@volume = volume
-			@source = source
-			@id = File.join(source.id, file)
+			@file = file
+			@id = File.join(file.source.id, file)
 		end
 
-		def from_hash(hash)
-			raise 'Expected a single key' unless hash.size == 1
-			@id = hash.keys.first
-			@sha1 = hash[id]['sha1']
+		def encode_with(coder)
+			coder.represent_map(nil, to_h)
 		end
 
-		def to_hash
+		def to_h
 			{id => {'volume' => volume.id, 'sha1' => sha1}}
 		end
-	end
+
+		def sha1
+			@sha1.nil? ? @sha1 = Digest::SHA1.file(File.join(volume.path, id)).to_s : @sha1 # TODO compute hash on file to stream copying to avoid extra pass though file contents
+		end
+
+	end # Stream
 
 end # Volume
 
 
 class Manifest
 
-	Version = 1
+	Version = 0
 
 	Glob = '*.yaml'
 
@@ -181,11 +217,14 @@ class Manifest
 		@project = project
 		@modified = false
 		@file = file
+		# Perform cleanup actions on rollback
+		# Note then setting this to true might leave the on-disk data in inconsistent state
+		@cleanup = true
 		if @file.nil?
 			@state = {
 				'version' => Version,
 				'session' => SecureRandom.uuid,
-				'stamp' => Time.now,
+				'mtime' => Time.now,
 				'sources' => {}
 			}
 		else
@@ -197,7 +236,7 @@ class Manifest
 
 	def store!
 		if modified?
-			time = @state['stamp'] = Time.now
+			time = @state['mtime'] = Time.now
 			@file = "#{time.to_i}.yaml"
 			@path = File.join(project.destination, @file)
 			raise 'Refuse to overwrite exising manifest' if File.exist?(@path)
@@ -215,14 +254,15 @@ class Manifest
 		end
 	end
 
-	def merge!(source, file, stream)
+	def merge!(file, stream)
+		source = file.source
 		sources[source.id] = contents = {'directory' => source.directory, 'files' => {}} if (contents = sources[source.id]).nil?
-		contents['files'][file] = {'stream' => stream.to_hash}
+		contents['files'][file] = {'stream' => stream}
 		@modified = true
 	end
 
 	def cleanup!
-		FileUtils.rm_rf(@path) if modified?
+		FileUtils.rm_rf(@path) if modified? && @cleanup
 	end
 
 end # Manifest
